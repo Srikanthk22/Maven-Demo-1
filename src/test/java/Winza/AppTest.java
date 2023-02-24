249ff692c35770c18f78ef5c34b8a8b10737395b
Sri K <srik@gmail.com>
2023-02-24 09:32:53 +0530

    Adding Maven Demo 1

---
+++ src/test/java/Winza/AppTest.java
@@ -0,0 +1,20 @@
+package Winza;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
